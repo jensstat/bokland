5f60736b10e6aa26abaa7fac8aad10b332016de4
jensstat <jensbergjensen@gmail.com>
2024-11-04 09:53:07 +0100

    deploy

--- src/routes/books/+page.server.js
+++ src/routes/books/+page.server.js
@@ -1,5 +1,5 @@
 // src/routes/+page.server.js
-import { loadBooks } from '../../lib/utils/loadbooks';
+import { loadBooks } from '../../lib/utils/loadBooks';
 
 export async function load() {
   const books = await loadBooks();
