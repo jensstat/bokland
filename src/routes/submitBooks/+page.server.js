b3f804936c2d81f65a4ffc5b0befd06a7d529dba
jensstat <jensbergjensen@gmail.com>
2024-11-04 09:58:40 +0100

    asdj

--- src/routes/submitBooks/+page.server.js
+++ src/routes/submitBooks/+page.server.js
@@ -1,5 +1,5 @@
 // src/routes/submitbooks/+page.server.js
-import { loadBooks } from '$lib/utils/loadBooks';
+import { loadBooks } from '../../lib/utils/loadBooks';
 
 export async function load() {
   const books = await loadBooks();
